b6508416863dcd50040107482957dcd84329ef8d
Sayonil Mitra <sayonilmitra@gmail.com>
2023-02-24 17:06:20 +0530

    fixed redirect issue after uploading picture

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,6 +1,7 @@
 import './UploadForm.css'
 import { useState } from 'react'
 import Header from './Header'
+import { useNavigate } from 'react-router-dom'
 
 function UploadForm() {
 
@@ -8,9 +9,14 @@ function UploadForm() {
     let [location, setLocation] = useState('')
     let [image, setImage] = useState('')
     let [description, setDescription] = useState('')
+    let [fileName, setFileName] = useState('')
+    let [message, setMessage] = useState('')
+    const navigate = useNavigate()
 
     function postImage(event) {
-        // event.preventDefault()
+        event.preventDefault()
+
+        setMessage("Upload Successful. Redirecting....")
 
         const monthNames = ["January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December"
@@ -32,11 +38,14 @@ function UploadForm() {
             headers: {
                 'Content-type': 'application/json; charset=UTF-8'
             }
+        }).then(() => {
+            navigate('/postview')
         })
 
     }
 
     function convertImage(event) {
+        setFileName(event.target.value)
         let file = event.target.files[0]
         let reader = new FileReader()
         reader.onloadend = function () {
@@ -48,23 +57,29 @@ function UploadForm() {
 
     return <>
         <Header />
-        <form className="upload-form" action='/postview'>
+        <form className="upload-form">
             <div className="upload-form-row">
-                <input type='file' onChange={(e) => convertImage(e)} />
+                <input id='fileName' value={fileName} />
+                <label htmlFor='fileInput'>
+                    Upload
+                    <input type='file' id='fileInput' onChange={(e) => convertImage(e)} />
+                </label>
+            </div>
+
+            <div className="upload-form-row">
+                <input type='text' placeholder="Author" onChange={(e) => setUserName(e.target.value)} />
             </div>
             <div className="upload-form-row">
-                <div className="upload-form-col">
-                    <input type='text' placeholder="Author" onChange={(e) => setUserName(e.target.value)} />
-                </div>
-                <div className="upload-form-col">
-                    <input type='text' placeholder="location" onChange={(e) => setLocation(e.target.value)} />
-                </div>
+                <input type='text' placeholder="location" onChange={(e) => setLocation(e.target.value)} />
             </div>
+
             <div className="upload-form-row">
                 <input type='text' placeholder="Description" onChange={(e) => setDescription(e.target.value)} />
             </div>
             <div>
                 <button type='submit' onClick={postImage}>Post</button>
+                <br />
+                {message === '' ? <></> : message}
             </div>
         </form >
     </>
